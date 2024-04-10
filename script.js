b9505f7e9a0e49c072797acf8e1136112654ddde
Daniel Chin <DanChin327@gmail.com>
2024-04-10 19:48:53 +0900

    completed drawing and colr functions

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 // DOMContentLoaded waits for HTML to load first so that JS doesn't break.
+let color = "black";
 
-console.log("HELLEO")
 document.addEventListener("DOMContentLoaded", function(){
-
-  console.log("Hello WORLD")
+  createBoard(32);
+  let btnPopUp = document.querySelector("#pop-up");
+  btnPopUp.addEventListener("click", function(){
+    let size = getSize();
+    createBoard(size);
+  })
 })
+
 function createBoard(size){
   let board = document.querySelector(".board");
   board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
@@ -14,7 +19,40 @@ function createBoard(size){
 
   for(let i = 0; i < numDivs; i++){
     let div = document.createElement("div");
-    div.style.backgroundColor = "white";
+    div.addEventListener("mouseover", colorDiv)
     board.insertAdjacentElement("beforeend", div);
   }
 };
+
+function getSize(){
+  let input = prompt("Give me grid size")
+  let message = document.querySelector("#message");
+  if(input == "") {
+    message.innerHTML = "Provide a number."
+  }
+  else if (input < 0 || input > 100) {
+    message.innerHTML = "Provide a number between 1 to 100"
+  }
+  else {
+    message.innerHTML = "You are ready to draw!"
+    return input;
+  }
+}
+
+function colorDiv(){
+  if(color == "random"){
+    this.style.backgroundColor = `hsl(${Math.random()*360}, 100%, 50%)`
+  }
+  else{
+    this.style.backgroundColor = 'black';
+  }
+}
+
+function setColor(colorChoice){
+  color = colorChoice;
+}
+
+function resetBoard(){
+  let divs = document.querySelectorAll("div")
+  divs.forEach((div) => div.style.backgroundColor = "white")
+}
